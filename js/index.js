20ceb232af915347f5c887861da7319f5c1db15b
ctk <775639849@qq.com>
2020-07-17 20:46:21 +0800

    123123

--- js/index.js
+++ js/index.js
@@ -5,11 +5,11 @@ window.onload = function() {
     body.addEventListener("click", function(event) {
         const top = event.clientY;
         const left = event.clientX;
-        const dot = document.createElement('div');
-        dot.className = dot;
-        dot.style.left = left + 'px';
-        dot.style.top = top + 'px';
-        body.appendChild(dot);
-        console.log(dot);
+        const d = document.createElement('div');
+        d.className = dot;
+        d.style.left = left + 'px';
+        d.style.top = top + 'px';
+        body.appendChild(d);
+        console.log(d);
 })
 }
