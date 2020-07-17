25cc1e3b4b37f6c8bc9182025db67445501b28fe
ctk <775639849@qq.com>
2020-07-17 20:09:39 +0800

    12312

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 window.onload = function() {
-    window.onclick = function () {
-        const circle = document.createElement('div')
-        circle.style = {
-            width: "50px",
-            height: "50px",
-            borderRadius: "25px",
-            position: "absolute",
-            background:"transparent",
-            animation: "sploosh 3s cubic-bezier(0.165, 0.85, 0.44, 1)"
-        }
-        const top = window.clientY;
-        const left = this.clientX;
-        console.log(top);
+    const circle = document.createElement('div')
+    circle.style = {
+        width: "50px",
+        height: "50px",
+        borderRadius: "25px",
+        position: "absolute",
+        background:"transparent",
+        animation: "sploosh 3s cubic-bezier(0.165, 0.85, 0.44, 1)"
     }
+    const body = document.querySelector("body")
+    body.addEventListener("onclick", function(event){
+        const top = event.clientY;
+        const left = event.clientX;
+        console.log(top);
+    })
 }
