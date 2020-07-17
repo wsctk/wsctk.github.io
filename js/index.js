baac6b8e4520e96491d80af56a2ca6185793c974
ctk <775639849@qq.com>
2020-07-17 20:30:43 +0800

    12313

--- js/index.js
+++ js/index.js
@@ -5,6 +5,6 @@ window.onload = function() {
     body.addEventListener("click", function(event) {
         const top = event.clientY;
         const left = event.clientX;
-        document.append('<div class="dot" style="top:' + top +'px;left: ' + left + 'px;"></div>')
+        body.append('<div class="dot" style="top:' + top +'px;left: ' + left + 'px;"></div>')
 })
 }
