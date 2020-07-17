29283507b97a3293f0f971fba17f6f893609431a
ctk <775639849@qq.com>
2020-07-17 19:59:47 +0800

    13212

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
-window.onload = function () {
-    console.log('123231')
+window.onload = function() {
+    window.onclick = function () {
+        const circle = document.createElement('div')
+        circle.style = {
+            width: "50px",
+            height: "50px",
+            borderRadius: "25px",
+            position: "absolute",
+            background:"transparent",
+            animation: "sploosh 3s cubic-bezier(0.165, 0.85, 0.44, 1)"
+        }
+        const top = this.clientY;
+        const left = this.clientX;
+        console.log(top);
+    }
 }
